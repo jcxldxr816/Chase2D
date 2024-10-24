994c6bdc0b51a7b7b78ed09c32ff717c37533d02
James Calder <154292961+jcxldxr816@users.noreply.github.com>
2024-10-24 15:49:32 -0500

    Initial Commit

---
+++ Chase2D/Chase2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chase2DApp.swift
+//  Chase2D
+//
+//  Created by James Calder on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Chase2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chase2DTests/Chase2DTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chase2DTests.swift
+//  Chase2DTests
+//
+//  Created by James Calder on 10/24/24.
+//
+
+import Testing
+@testable import Chase2D
+
+struct Chase2DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
